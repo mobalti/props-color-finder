bbe25a9985658fae5740243abb72c7b59d928a58
Mo Balti <horizonmedimp@gmail.com>
2023-03-09 20:37:22 +0100

    fix pwa manifest

--- vite.config.js
+++ vite.config.js
@@ -3,18 +3,20 @@ import { VitePWA } from 'vite-plugin-pwa';
 export default defineConfig({
   plugins: [
     VitePWA({
-      icons: [
-        {
-          src: '/image/icons-192.png',
-          type: 'image/png',
-          sizes: '192x192',
-        },
-        {
-          src: '/image/icons-512.png',
-          type: 'image/png',
-          sizes: '512x512',
-        },
-      ],
+      manifest: {
+        icons: [
+          {
+            src: '/image/icons-192.png',
+            type: 'image/png',
+            sizes: '192x192',
+          },
+          {
+            src: '/image/icons-512.png',
+            type: 'image/png',
+            sizes: '512x512',
+          },
+        ],
+      },
     }),
   ],
 });
